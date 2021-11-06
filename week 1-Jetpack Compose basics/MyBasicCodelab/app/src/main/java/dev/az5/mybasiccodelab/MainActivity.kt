5f14dbfb84518c4d9c0d380bee3f829c057e00d8
William Kim <azamara@me.com>
2021-11-07 03:23:25 +0900

    refactor(week1): Tweaking the UI

--- week 1-Jetpack Compose basics/MyBasicCodelab/app/src/main/java/dev/az5/mybasiccodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/MyBasicCodelab/app/src/main/java/dev/az5/mybasiccodelab/MainActivity.kt
@@ -3,11 +3,14 @@ package dev.az5.mybasiccodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.az5.mybasiccodelab.ui.theme.MyBasicCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,10 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyBasicCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                Greeting("Android")
             }
         }
     }
@@ -26,7 +26,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
 }
 
 @Preview(showBackground = true)
